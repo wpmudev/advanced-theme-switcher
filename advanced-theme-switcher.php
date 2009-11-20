1fccd393978e23b9b0d08acc734d9fd9e1c77293
austin <austin@f53fe46e-8c02-459b-9fdd-93e75771b278>
2009-11-20 00:39:52 +0000

    add in gpl notice
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/advanced-theme-switcher@74 f53fe46e-8c02-459b-9fdd-93e75771b278

--- advanced-theme-switcher.php
+++ advanced-theme-switcher.php
@@ -13,6 +13,24 @@ http://www.alexking.org/software/wordpress/
 
 */ 
 
+/* 
+Copyright 2007-2009 Incsub (http://incsub.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
 class ThemeSwitcherWidget extends WP_Widget {
 	function ThemeSwitcherWidget()
 	{
